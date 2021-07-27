5df35fea7955d450d6b19bd99c1a28eec0604c47
NovemForxuz <mherizpy@live.com>
2021-07-27 01:05:07 -0700

    Case study of Percentile Rank based on scores of students

--- HackerRank/src/Percentile_Rank.py
+++ HackerRank/src/Percentile_Rank.py
@@ -7,26 +7,30 @@ for index in range(1, n+1):
     dataset.append(int(input(f"Student {index}'s score: ")))
 
 
+# Defining a function to calculate percentile rank
 def find_percentile_rank(score):
     # cumulative frequency is the count of all scores less than or equal to score of interest
     cFreq = cumulative_frequency(score)
-    print(f"cFreq: {cFreq}")
     # frequency for the score of interest
     freq = dataset.count(score)
-    print(f"freq: {freq}")
+
     percentile_rank = (cFreq - (0.5 * freq)) / n * 100
 
     return percentile_rank
 
 
+# Defining a function to calculate cumulative frequency
 def cumulative_frequency(score):
-    cfreq = 0
-    sData = list(set(dataset))
+    cfreq = 0   # initialize
+    sData = list(set(dataset))  # distinct values
+
+    # loop through dataset(distinct) & count frequencies based on dataset(input)
     for i in range(len(sData)):
         if sData[i] <= score:
             cfreq += dataset.count(sData[i])
     return cfreq
 
 
+# Display result
 for data in dataset:
     print(f"Score: {data}, Percentile: {find_percentile_rank(data)}")
